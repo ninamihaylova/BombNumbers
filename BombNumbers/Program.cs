ee75572be213ae4c50217d85b727cf6185a6a09d
ninamihaylova <163305180+ninamihaylova@users.noreply.github.com>
2024-07-05 17:56:09 +0300

    Initial commit

---
+++ BombNumbers/Program.cs
@@ -0,0 +1,31 @@
+﻿class BombNumbers
+{
+    static void Main()
+    {
+        List<int> numbers = Console.ReadLine()
+            .Split()
+            .Select(int.Parse)
+            .ToList();
+        int[] bombParams = Console.ReadLine()
+            .Split()
+            .Select(int.Parse)
+            .ToArray();
+        int bombNumber = bombParams[0];
+        int bombPower = bombParams[1];
+
+        for (int i = 0; i < numbers.Count; i++)
+        {
+            if (numbers[i] == bombNumber)
+            {
+                int start = Math.Max(0, i - bombPower);
+                int end = Math.Min(numbers.Count - 1, i + bombPower);
+
+                numbers.RemoveRange(start, end - start + 1);
+                i = 0;
+            }
+        }
+
+        int sum = numbers.Sum();
+        Console.WriteLine(sum);
+    }
+}
